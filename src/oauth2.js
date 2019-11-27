109d6ea07dee2102a8e45fecae574f5b5e0a869f
lefherz <emil.lefherz@fau.de>
2019-11-27 03:01:49 +0100

    dummy functions for OAuth2 library

--- src/oauth2.js
+++ src/oauth2.js
@@ -4,65 +4,9 @@ var OAuthServer = require('express-oauth-server');
 var { asyncMiddleware } = require('./util');
 var { getEntry } = require('./database');
 
-const clients = [{ clientId: 'jkierjlwerWE', clientSecret: 'SGJKrJKIwdJKI4T908834njkfIO', redirectUris: ['https://support.delta.chat/'] }]
-
-//DB-----------------------------
-const Sequelize = require('sequelize');
-//https://sequelize.org/v5/manual
-
-const path = require('path')
-const sequelize = new Sequelize(`sqlite:${path.resolve(path.join(__dirname, '../data/oauth2db.sqlite'))}`)
-const Model = Sequelize.Model;
-
-// class AuthorizationCode extends Model { }
-// AuthorizationCode.init({
-//     authorization_code: {
-//         type: Sequelize.STRING,
-//         allowNull: false
-//     },
-//     expires_at: Sequelize.DATE,
-//     redirect_uri: Sequelize.STRING,
-//     scope: Sequelize.STRING,
-//     client_id: Sequelize.INTEGER,
-//     user_id: Sequelize.INTEGER
-// }, {
-//     sequelize,
-//     modelName: 'authorizationcode'
-//     // options
-// })
-
-// class AccessToken extends Model { }
-// AccessToken.init({
-//     access_token: Sequelize.STRING,
-//     expires_at: Sequelize.DATE,
-//     scope: Sequelize.STRING,
-//     client_id: Sequelize.INTEGER,
-//     user_id: Sequelize.INTEGER
-// }, {
-//     sequelize,
-//     modelName: 'authorizationcode'
-//     // options
-// })
-
-
-
-var tokens = [];
-
-
-
-
-//end_DB-----------------------------
-
 var router = Router();
 router.oauth = new OAuthServer({
     model: {
-        getClient: function (clientId, clientSecret) {
-            var clients = this.clients.filter(function (client) {
-                return client.clientId === clientId && client.clientSecret === clientSecret;
-            });
-
-            return clients.length ? clients[0] : false;
-        },
         saveAuthorizationCode: function (code, client, user) {
             // imaginary DB queries
             let authCode = {
