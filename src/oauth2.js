07d476783992987ef011188906b89c639e16345b
Simon Laux <mobile.info@simonlaux.de>
2019-11-27 21:36:41 +0100

    try

--- src/oauth2.js
+++ src/oauth2.js
@@ -58,7 +58,7 @@ router.all('/authorize', authGuard, asyncMiddleware(async function (req, res) {
     }
 
     // todo GENERATE AND SAVE the auth code
-    const auth_code = uuid().replace("-","")
+    const auth_code = (`${uuid()}`.replace("-",""))
     console.log(auth_code)
     await insertAuthCode(auth_code, res.locals.contactId)
 
