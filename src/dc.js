8b2d79781baa3e6752123626962780a255006b9e
pabzm <pabzm-github@mv.so36.net>
2019-11-28 12:49:44 +0000

    Rebuild without sockets, less callbacks and session middleware.
    The state is stored in a session, which is identified by a signed
    cookie. No data is manually stored in the DB anymore except the list of
    public chats.
    The browser polls for status updates every 5 seconds.
    The server asks DC directly, if the wanted group has other members than
    the bot. Thus no callbacks are necessary to inform the app, or the
    browser of the successful join.

--- src/dc.js
+++ src/dc.js
@@ -3,18 +3,9 @@ const DeltaChat = require('deltachat-node')
 const C = require('deltachat-node/constants')
 const path = require('path')
 
-let listeners = []
-global.listeners = listeners
-function listenOnGroupchange(id, cb) {
-    listeners.push({ id, cb })
-}
-function onGroupChange(id, newContactId) {
-    const i = listeners.findIndex(({ id }) => id === id)
-    if (i !== -1) {
-        const cb = listeners[i].cb
-        listeners.splice(i, 1)
-        cb(newContactId)
-    }
+function getNewContactInGroup(chat_id) {
+    const contacts = dc.getChatContacts(chat_id)
+    return contacts.filter(cid => cid !== C.DC_CONTACT_ID_SELF)[0]
 }
 
 const {
@@ -29,21 +20,23 @@ const dc = new DeltaChat()
 const handleDCMessage = async (dc, chatId, msgId) => {
     // handle dc message
     var msg = dc.getMessage(msgId)
-    console.log("got a message:", msg)
-
+    console.log(`Got a message for chat ${chatId}: ${msg.getText()}`)
+    
     switch (msg.getText()) {
-        case '/publish':
-            saveChat(chatId).then(_ => {
-                console.log("saved chat", chatId);
-                // TODO: send acknowledgement to group
-            })
-            break
-        case '/unpublish':
-            deleteChat(chatId).then(_ => {
-                console.log("deleted chat", chatId);
-                // TODO: send acknowledgement to group
-            })
-            break
+      case '/publish':
+        console.log("Saving chat as public group")
+        saveChat(chatId).then(_ => {
+            console.log("Saved chat, sending acknowledging message", chatId);
+            dc.sendMessage(chatId, "OK, chat was opened to public subscription through the web login app. ✓")
+        })
+        break
+      case '/unpublish':
+        console.log("Deleting chat from public groups")
+        deleteChat(chatId).then(_ => {
+            console.log("Deleted chat, sending acknowledging message", chatId);
+            dc.sendMessage(chatId, "OK, chat was removed from public subscription. ✓")
+        })
+        break
     }
 
 }
@@ -58,42 +51,6 @@ dc.on('DC_EVENT_MSGS_CHANGED', (chatId, msgId) => {
 dc.on('DC_EVENT_INCOMING_MSG', (...args) => handleDCMessage(dc, ...args))
 // dc.on('ALL', console.log.bind(null, 'core |'))// for debugging
 
-
-dc.on('DC_EVENT_CHAT_MODIFIED', function (chat_id) {
-    console.log(`Event Chat Modified for group ${chat_id}`)
-    // Try 3 times with delay because deltachat might be slower updating the
-    // group state than us.
-    setTimeout(_ => {
-        if (!findAndHandleContactId(chat_id)) {
-            console.log("Trying again in 1s")
-            setTimeout(_ => {
-                if (!findAndHandleContactId(chat_id)) {
-                    setTimeout(_ => {
-                        if (!findAndHandleContactId(chat_id)) {
-                            console.error("Error: could not find new contact id, cancelling!")
-                        }
-                    }, 1000)
-                }
-            }, 1000)
-        }
-    }, 1000)
-})
-
-function findAndHandleContactId(chat_id) {
-    console.log("Looking to find a new contact id in the group")
-    const contacts = dc.getChatContacts(chat_id)
-    const newContactId = contacts.filter(cid => cid !== C.DC_CONTACT_ID_SELF)[0]
-    console.log({ chat_id, contacts, newContactId })
-    if (newContactId == null) {
-        console.error("no new contact id found")
-        return false
-    }
-    console.log(`found new contact id: ${newContactId}`)
-    onGroupChange(chat_id, newContactId)
-    // TODO: send notice to contact that they may delete this group now
-    // TODO: remove self from group
-    return true
-}
 
 dc.open(path.join(__dirname, '../data'), () => {
     if (!dc.isConfigured()) {
@@ -114,5 +71,5 @@ process.on('exit', () => {
 
 module.exports = {
     dc,
-    listenOnGroupchange
+    getNewContactInGroup
 }
--- src/main.js
+++ src/main.js
@@ -1,17 +1,13 @@
 const { router } = require("./oauth2");
 
 const config = require('./config')
-const { dc, listenOnGroupchange } = require('./dc')
+const { dc, getNewContactInGroup } = require('./dc')
 const path = require('path')
 const C = require('deltachat-node/constants')
 const uuid = require('uuid/v4');
 const { asyncMiddleware } = require('./util');
 
 const {
-    insertEntry,
-    getEntry,
-    deleteEntry,
-    getChat,
     getChats,
 } = require('./database')
 
@@ -19,116 +15,136 @@ const {
 const app = require('express')();
 exports.app = app;
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const ejs = require('ejs')
 const qrcode_generator = require('qrcode')
+var ejs = require('ejs')
+var session = require('express-session');
 
-const cookieParser = require("cookie-parser");
-app.use(cookieParser());
+app.use(session({
+    secret: uuid(), // use a new secret on every startup of the app
+    saveUninitialized: true,
+    resave: false,
+    name: 'delta-login-session',
+    cookie: {
+        httpOnly: false, // allow to use the cookie for JavaScript-requests, too.
+        sameSite: true,
+    }
+}));
+
 
 app.get('/', asyncMiddleware(async function (req, res) {
-    console.log(req.cookies.token)
-    const entry = req.cookies.token && await getEntry(req.cookies.token)
-    if (entry) {
-        console.log("_>", entry)
-        let chatIds = await getChats() || []
-        let chats = chatIds.map(chatId => {
-            return {
-                "chatId": chatId,
-                "chatName": dc.getChat(chatId).getName(),
-                "already_joined": dc.isContactInChat(chatId, entry.contactId)
-            }
-        })
+    console.log("session:", req.session)
+    const group_name = `LoginBot group (${uuid().slice(0, 4)})`
+    console.log("new group name:", group_name)
+    const group_id = dc.createUnverifiedGroupChat(group_name)
+    console.log("new group_id:", group_id)
+    const qr_data = dc.getSecurejoinQrCode(group_id)
+    const qr_code_data_url = await qrcode_generator.toDataURL(qr_data)
+    const content = await ejs.renderFile(
+        path.join(__dirname, '../web/new_user.ejs').toString(),
+        { qr_code_data_url, qr_data }
+    )
+    req.session.groupId = group_id
+    res.send(content)
+}));
 
-        const content = await ejs.renderFile(
-            path.join(__dirname, '../web/loggedin.ejs').toString(),
-            {
-                address: dc.getContact(entry.contactId).toJson().address,
-                chats: chats,
-                successMessage: req.cookies.successMessage
-            }
-        )
-        res.clearCookie('successMessage')
-        res.send(content)
+
+app.get('/checkStatus', asyncMiddleware(async function (req, res) {
+    if (!req.session.groupId) {
+        return res.redirect('/')
+    }
+    console.log("Looking for new contact in group", req.session.groupId)
+    let newContactId = getNewContactInGroup(req.session.groupId)
+    console.log("newContactId in group:", newContactId)
+    if (newContactId) {
+        req.session.contactId = newContactId
+        res.send("OK")
     } else {
-        res.sendFile(path.join(__dirname, '../web/new_user.html'));
-    }
-}));
-
-
-app.get('/logout', asyncMiddleware(async function (req, res) {
-    const entry = req.cookies.token && await getEntry(req.cookies.token)
-
-    if (entry) {
-        await deleteEntry(req.cookies.token)
+        res.send('Not yet...')
     }
 
-    res.clearCookie('token')
-    res.redirect('/');
-}));
+}))
 
 
-app.get('/joinGroup/:chatId', asyncMiddleware(async function (req, res) {
+
+const ensureAuthenticated = function (req, res, next) {
+    console.log("Checking authentication")
+    if (!req.session.contactId) {
+        console.log("Unauthenticated request, redirecting to /")
+        return res.redirect('/')
+    } else {
+        next()
+    }
+}
+
+
+
+app.get('/dashboard', ensureAuthenticated, asyncMiddleware(async function (req, res) {
+    let contactId = req.session.contactId
+    console.log(`Rendering dashboard for contactId ${contactId}`)
+    let chatIds = await getChats() || []
+    let chats = chatIds.map(chatId => {
+        return {
+            "chatId": chatId,
+            "chatName": dc.getChat(chatId).getName(),
+            "already_joined": dc.isContactInChat(chatId, contactId)
+        }
+    })
+
+    const content = await ejs.renderFile(
+        path.join(__dirname, '../web/loggedin.ejs').toString(),
+        {
+            address: dc.getContact(contactId).toJson().address,
+            chats: chats,
+            successMessage: req.session.successMessage
+        }
+    )
+    req.session.successMessage = ''
+    res.send(content)
+}))
+
+
+app.get('/logout', function (req, res, next) {
+    req.session.destroy((err) => {
+        if (err) throw err
+        else next()
+    })
+}, function (req, res) {
+    console.log("Session destroyed, redirecting to /")
+    res.redirect('/')
+});
+
+
+app.get('/joinGroup/:chatId', ensureAuthenticated, asyncMiddleware(async function (req, res) {
+    console.log("Request to joinGroup")
     const chatId = req.params.chatId
     const chat = chatId && dc.getChat(chatId)
-    const login = req.cookies.token && await getEntry(req.cookies.token)
 
-    if (chat && login) {
-        console.log(`Adding contact ${login.contactId} to group ${chatId}`);
-        dc.addContactToChat(chatId, login.contactId)
-        res.cookie('successMessage', `✓ You joined group ${chat.getName()}`)
+    if (!chat) {
+        console.log("The requested chat_id does not exist")
+        res.session.successMessage = "You'd have to create that group first, please. It does not exist yet :)"
+        res.redirect('/')
+        return
     }
+
+    // Shouldn't happen, but better check twice.
+    if (!req.session.contactId) {
+        console.log("Error: not contactId in session!")
+        return res.redirect('/')
+    }
+
+    console.log(`Adding contact ${req.session.contactId} to group ${chatId}`);
+    dc.addContactToChat(chatId, req.session.contactId)
+    res.session.successMessage = `✓ You joined group ${chat.getName()}`
     res.redirect('/')
 }));
 
 
-io.on('connection', function (socket) {
-    console.log('a user connected');
-    var login_token;
-
-    // configure dc work around
-    socket.on('getQR', (fn) => {
-        const s = socket;  // voodoo to trick the garbage collector
-        // Get QR code
-        let group_name = `LoginBot group (${uuid().slice(0, 4)})`
-        const login_group_id = dc.createUnverifiedGroupChat(group_name)
-
-        listenOnGroupchange(login_group_id, (newContactId) => {
-            console.log("new contact was added to group, saving token into DB")
-            const token = uuid()
-            insertEntry(token, newContactId).then(_ => {
-                console.log("notifying socket about verified token")
-                // send token on verification. toString() apparently helps to
-                // avoid garbage collection of the token.
-                login_token = token.toString()
-                s.emit("verified", login_token)
-            }, console.error)
-        })
-
-        const qr_data = dc.getSecurejoinQrCode(login_group_id)
-        qrcode_generator.toDataURL(qr_data, function (err, url) {
-            fn(url, qr_data);
-        })
-    });
-
-    socket.on('poll', function () {
-        if (login_token) {
-            s.emit("verified", login_token)
-        }
-    });
-
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-    });
-});
-
 app.use('/oauth2', router)
 
-app.get('/styles.css',(_req, res)=>{res.sendFile(path.join(__dirname, '../web/styles.css'))} )
+app.get('/styles.css', (_req, res) => { res.sendFile(path.join(__dirname, '../web/styles.css')) })
 
 const PORT = process.env.PORT || 3000
 
 http.listen(PORT, function () {
     console.log(`listening on *:${PORT}`);
 });
-
